a2d8623cd806e65ec4d6feb343792fc77a69f41e
faiza <faiza@Faizan>
2025-10-08 11:40:13 +0530

    The first commit

---
+++ MavenProjectM1/src/test/java/com/maven/demo/SampleTest.java
@@ -0,0 +1,10 @@
+package com.maven.demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void case1() {
+		System.out.println("This is case1 from SampleTest of demo.");
+	}
+}
---
+++ MavenProjectM1/src/test/java/com/maven/sample/DemoTest.java
@@ -0,0 +1,10 @@
+package com.maven.sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void case2() {
+		System.out.println("This is case2 from DemoTest of sample.");
+	}
+}
